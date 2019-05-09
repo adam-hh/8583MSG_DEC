5e0e2b96c9d9a9dad48f7987440366a0f3675457
adam <409520259@qq.com>
2019-05-09 15:39:36 +0800

    009

---
+++ src/version.h
@@ -0,0 +1,18 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define PRODUCT_ICON           "bugger.ico" // 图标
+
+#define FILE_VERSION           4,0,2,666   // 文件版本
+#define FILE_VERSION_STR       "4.0.2.666"
+#define PRODUCT_VERSION        4,0,2,666   // 产品版本
+#define PRODUCT_VERSION_STR    "4.0.2.666"
+#define COMPANY_NAME           "Digia"
+#define INTERNAL_NAME          "gui.exe"
+#define FILE_DESCRIPTION       "Qt 64"  // 文件说明
+#define LEGAL_COPYRIGHT        "Copyright 2008-2019 HH All rights reserved." // 版权
+#define ORIGINAL_FILE_NAME     "gui.exe"    // 原始文件名
+#define PRODUCT_NAME           "Qt 64"        // 产品名称
+#define ORGANIZATION_DOMAIN    "https://github.com/adam-hh/8583MSG_DEC"  // 域名
+
+#endif // VERSION_H
